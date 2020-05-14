4d64936c1223deb6ea0d3200483433c6cc110f00
Without Boats <woboats@gmail.com>
2020-05-14 17:58:14 +0200

    Remove async-std dev-dependency
    Since we depend on futures now and futures provides a block_on executor
    and that's all we need from async-std, just use the one in futures.

--- tests/ring-read.rs
+++ tests/ring-read.rs
@@ -11,7 +11,7 @@ const ASSERT: &[u8] = b"But this formidable power of death -";
 fn read_file() {
     let file = File::open("props.txt").unwrap();
     let mut file: Ring<File, _> = Ring::new(file, &DRIVER);
-    async_std::task::block_on(async move {
+    futures::executor::block_on(async move {
         let mut buf = vec![0; 4096];
         assert!(file.read(&mut buf).await.is_ok());
         assert_eq!(&buf[0..ASSERT.len()], ASSERT);
