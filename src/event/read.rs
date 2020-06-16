fb4e885a83ae1b71e9f211d9aa7a936935d3b4f9
Dmitriy <d.razumovskiy@kupibilet.ru>
2020-06-16 20:38:05 +0300

    Add offset to read and write event.

--- src/event/read.rs
+++ src/event/read.rs
@@ -6,19 +6,20 @@ use super::{Event, Cancellation};
 
 /// A basic read event.
 pub struct Read<'a, T> {
-    pub io: &'a mut T,
+    pub io: &'a T,
     pub buf: Vec<u8>,
+    pub offset: usize
 }
 
 impl<'a, T: AsRawFd + Unpin> Read<'a, T> {
-    pub fn new(io: &'a mut T, buf: Vec<u8>) -> Read<T> {
-        Read { io, buf }
+    pub fn new(io: &'a T, buf: Vec<u8>, offset: usize) -> Read<T> {
+        Read { io, buf, offset }
     }
 }
 
 impl<'a, T: AsRawFd + Unpin> Event for Read<'a, T> {
     unsafe fn prepare(&mut self, sqe: &mut iou::SubmissionQueueEvent<'_>) {
-        sqe.prep_read(self.io.as_raw_fd(), &mut self.buf[..], 0);
+        sqe.prep_read(self.io.as_raw_fd(), &mut self.buf[..], self.offset);
     }
 
     fn cancellation(this: &mut ManuallyDrop<Self>) -> Cancellation {
--- tests/basic-read.rs
+++ tests/basic-read.rs
@@ -8,8 +8,8 @@ const ASSERT: &[u8] = b"But this formidable power of death -";
 
 #[test]
 fn read_file() {
-    let mut file = File::open("props.txt").unwrap();
-    let read: Read<'_, File> = Read::new(&mut file, vec![0; 4096]);
+    let file = File::open("props.txt").unwrap();
+    let read: Read<'_, File> = Read::new(&file, vec![0; 4096], 0);
     let (read, result) = futures::executor::block_on(Submission::new(read, demo::driver()));
     assert!(result.is_ok());
     assert_eq!(&read.buf[0..ASSERT.len()], ASSERT);
