64b0e1683df4fb9278013737d913fdc90192e8bf
Without Boats <woboats@gmail.com>
2020-09-25 17:29:48 +0200

    Replace the Buffer internals to be safer.

---
+++ src/buf/data.rs
@@ -0,0 +1,100 @@
+use std::any::{Any, TypeId};.
+use std::mem::ManuallyDrop;
+
+use crate::Cancellation;
+
+#[derive(Default)]
+pub struct Data {
+    inner: Option<Inner>
+}
+
+impl Data {
+    pub fn alloc_bytes(&mut self, len: usize) -> &mut [u8] {
+        if self.inner.is_none() {
+            self.inner = Some(Inner::Buffer(vec![0; len].into_boxed_slice()));
+        }
+        self.inner.as_mut().unwrap().bytes_mut().unwrap()
+    }
+
+    pub fn alloc<T: UringData>(&mut self, data: T) -> &mut T  {
+        if self.inner.is_none() {
+            self.inner = Some(Inner::Object(Box::new(data)));
+        }
+        self.inner.as_mut().unwrap().downcast().unwrap()
+    }
+
+    pub fn as_bytes(&self) -> Option<&[u8]> {
+        self.inner.as_ref().and_then(|inner| inner.bytes())
+    }
+
+    pub fn cancellation(&mut self) -> Cancellation {
+        self.inner.take().map_or_else(Cancellation::null, Inner::cancellation)
+    }
+}
+
+enum Inner {
+    Buffer(Box<[u8]>),
+    Object(Box<dyn UringData>),
+}
+
+impl Inner {
+    fn downcast<T: UringData>(&mut self) -> Option<&mut T> {
+        match self {
+            Inner::Object(object)   => object.downcast(),
+            Inner::Buffer(_)        => None,
+        }
+    }
+
+    fn bytes_mut(&mut self) -> Option<&mut [u8]> {
+        match self {
+            Inner::Buffer(bytes)    => Some(&mut bytes[..]),
+            Inner::Object(_)        => None,
+        }
+    }
+
+    fn bytes(&self) -> Option<&[u8]> {
+        match self {
+            Inner::Buffer(bytes)    => Some(&bytes[..]),
+            Inner::Object(_)        => None,
+        }
+    }
+
+    fn cancellation(self) -> Cancellation {
+        match self {
+            Inner::Buffer(bytes)    => {
+                let mut bytes = ManuallyDrop::new(bytes);
+                unsafe { Cancellation::buffer(bytes.as_mut_ptr(), bytes.len()) }
+            }
+            Inner::Object(object)   => {
+                let mut object = ManuallyDrop::new(object);
+                unsafe { object.cancellation() }
+            }
+        }
+    }
+}
+
+pub trait UringData: Send + Sync + Any {
+    /// # Safety
+    ///
+    /// This method must logically take ownership of `self`.
+    unsafe fn cancellation(&mut self) -> Cancellation;
+}
+
+impl dyn UringData + 'static {
+    fn downcast<T: UringData>(&mut self) -> Option<&mut T> {
+        if Any::type_id(self) == TypeId::of::<T>() {
+            unsafe { Some(&mut *(self as *mut dyn UringData as *mut T)) }
+        } else {
+            None
+        }
+    }
+}
+
+impl UringData for libc::statx {
+    unsafe fn cancellation(&mut self) -> Cancellation {
+        unsafe fn callback(ptr: *mut (), _: usize) {
+            drop(Box::from_raw(ptr as *mut libc::statx));
+        }
+        Cancellation::new(self as *mut libc::statx as *mut (), 0, callback)
+    }
+}
---
+++ src/buf/mod.rs
@@ -0,0 +1,59 @@
+mod data;
+
+use std::cmp;
+use std::io;
+use std::mem;
+use std::task::Poll;
+
+use futures_core::ready;
+
+use crate::Cancellation;
+
+use data::Data;
+
+pub struct Buffer {
+    data: Data,
+    pos: u32,
+    cap: u32,
+}
+
+impl Buffer {
+    pub fn new() -> Buffer {
+        Buffer {
+            data: Data::default(),
+            pos: 0,
+            cap: 0,
+        }
+    }
+
+    pub fn buffered_from_read(&self) -> &[u8] {
+        self.data.as_bytes().map_or(&[], |data| &data[self.pos as usize..self.cap as usize])
+    }
+
+    pub fn fill_buf(&mut self, fill: impl FnOnce(&mut [u8]) -> Poll<io::Result<u32>>)
+        -> Poll<io::Result<&[u8]>>
+    {
+        if self.pos >= self.cap {
+            self.cap = ready!(fill(self.data.alloc_bytes(4096 * 2)))?;
+            self.pos = 0;
+        }
+        Poll::Ready(Ok(self.buffered_from_read()))
+    }
+
+    pub fn consume(&mut self, amt: usize) {
+        self.pos = cmp::min(self.pos + amt as u32, self.cap);
+    }
+
+    pub fn clear(&mut self) {
+        self.pos = 0;
+        self.cap = 0;
+    }
+
+    pub fn as_statx(&mut self) -> &mut libc::statx {
+        self.data.alloc(unsafe { mem::zeroed() })
+    }
+
+    pub fn cancellation(&mut self) -> Cancellation {
+        self.data.cancellation()
+    }
+}
