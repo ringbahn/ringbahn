38c8867825ca1991d9d804e6fb613295001bf4a0
Without Boats <woboats@gmail.com>
2020-10-01 14:28:41 +0200

    Remove unneeded helper methods in Buf

--- src/buf.rs
+++ src/buf.rs
@@ -15,14 +15,20 @@ pub struct Buffer {
 
 impl Buffer {
     pub fn buffered_from_read(&self) -> &[u8] {
-        self.as_bytes().map_or(&[], |data| &data[self.pos as usize..self.cap as usize])
+        self.data.as_deref().map_or(&[], |data| &data[self.pos as usize..self.cap as usize])
     }
 
     pub fn fill_buf(&mut self, fill: impl FnOnce(&mut [u8]) -> Poll<io::Result<u32>>)
         -> Poll<io::Result<&[u8]>>
     {
+        const CAPACITY: usize = 4096 * 2;
+
         if self.pos >= self.cap {
-            self.cap = ready!(fill(self.alloc_bytes(4096 * 2)))?;
+            if self.data.is_none() {
+                self.data = Some(vec![0; CAPACITY].into_boxed_slice());
+            }
+
+            self.cap = ready!(fill(self.data.as_deref_mut().unwrap()))?;
             self.pos = 0;
         }
         Poll::Ready(Ok(self.buffered_from_read()))
@@ -43,17 +49,5 @@ impl Buffer {
 
     pub fn cancellation(&mut self) -> Cancellation {
         Cancellation::from(self.data.take())
-    }
-
-    fn as_bytes(&self) -> Option<&[u8]> {
-        self.data.as_deref()
-    }
-
-    fn alloc_bytes(&mut self, cap: usize) -> &mut [u8] {
-        if self.data.is_none() {
-            self.data = Some(vec![0; cap].into_boxed_slice());
-        }
-
-        self.data.as_deref_mut().unwrap()
     }
 }
